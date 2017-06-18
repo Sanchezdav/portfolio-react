f2dfd169f390940f1ba29a16226c9c61c0a6ba2f
David Sanchez <sanchez.dav90@gmail.com>
2017-06-18 13:42:59 -0500

    Initial commit with react router added

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Home extends Component {
+  static propTypes = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Hi home!!!</h1>
+      </div>
+    );
+  }
+}
