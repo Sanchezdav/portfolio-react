86e633fe0bfc27a6dc0180bbd3cccc26be686e58
David Sanchez <sanchez.dav90@gmail.com>
2017-06-18 15:32:51 -0500

    Styled components added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,23 @@
 import React, { Component, PropTypes } from 'react';
+import styled from 'styled-components';
+import Container from '../theme/grid/Container'
+
+const Image = styled.img`
+  width: 100%;
+`;
 
 export default class Home extends Component {
   static propTypes = {};
 
   render() {
     return (
-      <div>
+      <Container>
         <h1>Hi home!!!</h1>
-      </div>
+        <Image src={require('../assets/hero.gif')} alt="hero-image" />
+        <p>
+          Heirloom selvage stumptown gochujang health goth pinterest. Mlkshk meh man bun, fingerstache distillery kogi ugh cliche mumblecore paleo. 90's la croix artisan williamsburg, synth lyft intelligentsia yr ugh pickled poke listicle. Disrupt direct trade mumblecore fingerstache heirloom. Hoodie migas four dollar toast, meggings DIY vinyl prism hell of. Ramps pinterest kickstarter, meditation lyft pok pok semiotics tofu slow-carb wayfarers disrupt chicharrones neutra aesthetic fashion axe. Bushwick ennui cloud bread williamsburg banh mi. PBR&B brooklyn franzen sriracha. Selfies woke tbh, distillery 8-bit meh try-hard. Small batch fixie viral lomo kitsch cornhole paleo. Irony you probably haven't heard of them celiac vegan church-key, kitsch sriracha. Forage semiotics poke chambray. Put a bird on it DIY shoreditch master cleanse try-hard, iPhone locavore cliche af irony normcore chartreuse.
+        </p>
+      </Container>
     );
   }
 }
---
+++ src/theme/globalStyle.js
@@ -0,0 +1,10 @@
+import { injectGlobal } from 'styled-components';
+
+/* eslint-disable */
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+
+  body{
+    font-family: 'Roboto', sans-serif;
+  }
+`
